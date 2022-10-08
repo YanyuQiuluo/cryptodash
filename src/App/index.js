e34eb04b91f8ff8d636b4a340335f48dbd901931
Yue Zhou <yzhou209@sheffield.ac.uk>
2022-10-08 17:27:12 +0100

    Add App Layout

--- src/App/index.js
+++ src/App/index.js
@@ -1,25 +1,13 @@
 import WelcomeMessage from './WelcomeMessage';
 import './App.css';
-//import styled,{css} from 'styled-components';
-
-// const  MyButton= styled.div`
-//   color: green;
-//   ${props => props.primary && css`
-//     color: palevioletred;
-//   `}
-// `
-//
-// const TomatoButton = styled(MyButton)`
-//   color: tomato;
-//   border-color: tomato;
-// `;
+import AppLayout from "./AppLayout";
 
 
 function App() {
   return (
-      <div>
+      <AppLayout>
     <WelcomeMessage/>
-      </div>
+      </AppLayout>
   );
 }
 
