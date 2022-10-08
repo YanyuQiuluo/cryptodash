1b61a51fbb14a0e32f78824eab5955eafec78186
Yue Zhou <yzhou209@sheffield.ac.uk>
2022-10-08 17:39:15 +0100

    Add AppBar and  grid

--- src/App/index.js
+++ src/App/index.js
@@ -1,11 +1,13 @@
 import WelcomeMessage from './WelcomeMessage';
 import './App.css';
 import AppLayout from "./AppLayout";
+import AppBar from "./AddBar";
 
 
 function App() {
   return (
       <AppLayout>
+        <AppBar/>
     <WelcomeMessage/>
       </AppLayout>
   );
