d861ea21ac55a98047395d96da37fbefe8c27de9
Yue Zhou <yzhou209@sheffield.ac.uk>
2022-10-10 04:27:04 +0100

    Add coin tile

--- src/App/index.js
+++ src/App/index.js
@@ -10,7 +10,7 @@ function App() {
   return (
       <AppLayout>
           <AppProvider>
-            <AppBar/>
+              <AppBar/>
               <Content>
                   <Settings/>
               </Content>
