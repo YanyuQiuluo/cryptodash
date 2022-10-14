1ac6d0836f65da6e92fb8bc74c458aa5a86681b0
Yue Zhou <yzhou209@sheffield.ac.uk>
2022-10-14 03:36:45 +0100

    Add chartsSelect

--- src/App/AppProvider.js
+++ src/App/AppProvider.js
@@ -18,6 +18,7 @@ export class AppProvider extends React.Component{
         this.state = {
             page:'dashboard',
             favorites:['BTC','ETH','XMR','DOGE','BNB'],
+            timeInterval:'months',
             ...this.savedSettings(),
             setPage:this.setPage,
             addCoin:this.addCoin,
@@ -25,7 +26,8 @@ export class AppProvider extends React.Component{
             isInFavorites:this.isInFavorites,
             confirmFavorites: this.confirmFavorites,
             setFilteredCoins: this.setFilteredCoins,
-            setCurrentFavorite: this.setCurrentFavorite
+            setCurrentFavorite: this.setCurrentFavorite,
+            changeChartSelect: this.changeChartSelect
         }
     }
 
@@ -54,7 +56,7 @@ export class AppProvider extends React.Component{
         let historical = [{
             name: this.state.currentFavorite,
             data: results.map((ticker, index) =>[
-                moment().subtract({months:TIME_UNITS - index}).valueOf(),
+                moment().subtract({[this.state.timeInterval]:TIME_UNITS - index}).valueOf(),
                 ticker.USD
                 ])
         }]
@@ -83,7 +85,7 @@ export class AppProvider extends React.Component{
                     this.state.currentFavorite,
                     ['USD'],
                     moment()
-                        .subtract({months:units})
+                        .subtract({[this.state.timeInterval]:units})
                         .toDate()
                 )
             )
@@ -149,6 +151,11 @@ export class AppProvider extends React.Component{
     setPage = page => this.setState({page})
 
     setFilteredCoins = (filteredCoins) => this.setState({filteredCoins})
+
+    changeChartSelect = (value) => {
+        console.log(value);
+        this.setState({timeInterval: value, historical:null}, this.fetchHistorical);
+    }
 
     render() {
         return(
--- src/DashBoard/PriceChart.js
+++ src/DashBoard/PriceChart.js
@@ -5,14 +5,23 @@ import highcharts from 'highcharts'
 import ReactHighcharts from 'highcharts-react-official';
 import highchartsConfig from "./HighchartsConfig";
 import HighChartsTheme from "./HighChartsTheme";
+import ChartSelect from './ChartSelect'
 
 highcharts.setOptions(HighChartsTheme());
 
 export default function (){
     return (
         <AppContext.Consumer>
-            {({historical}) =>
+            {({historical, changeChartSelect}) =>
                 <Tile>
+                    <ChartSelect
+                        defaultValue={"months"}
+                        onChange={e => changeChartSelect(e.target.value)}
+                    >
+                        <option value="days"> Days </option>
+                        <option value="weeks"> Weeks </option>
+                        <option value="months"> Months </option>
+                    </ChartSelect>
                     {historical ?
                         <ReactHighcharts
                         highcharts={highcharts}
